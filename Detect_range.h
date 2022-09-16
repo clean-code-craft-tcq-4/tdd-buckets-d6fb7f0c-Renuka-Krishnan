0e488961a454469f280a8a4ffc2522d31ecb502a
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-09-16 17:12:51 +0530

    Update Detect_range.h

--- Detect_range.h
+++ Detect_range.h
@@ -1,1 +1,6 @@
- void Detect_range(char data, int* reading, int size);
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+void Detect_range(char data, int* reading, int size);
