fa480b11d2a32b98885a77439fb5bd774305d199
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-09-16 17:01:50 +0530

    Create test_range.c

---
+++ test_range.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "assert.h"
+#include <string.h>
+#include "Detect_range.h"
+
+int main()
+{
+  char Data[50]= {0};
+  int range[12] = {0,1,1,2,4,3,0,6,7,9,10,11};
+  char* expectedValue = "1-4, 5\n 6-7,2\n 9-11, 3\n";
+   
+  Detect_range(Data, range, (sizeof(range)/sizeof(int)));
+  
+  {
+    assert(!strcmp(Data, expectedValue);
+  }
+}
